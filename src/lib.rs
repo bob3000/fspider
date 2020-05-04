a661ca7efac714dbee04278ed5b81eea89af7b3a
Robin Kautz <mail@bob3000.net>
2020-05-04 17:02:35 +0200

    working recursion with `async_recursion`

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,39 @@
 use async_recursion::async_recursion;
-use async_std::fs;
+use async_std::fs::{self, DirEntry};
 use async_std::path::Path;
 use async_std::prelude::*;
 use async_std::task;
+use futures::future::Future;
 use std::cmp::Eq;
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::Debug;
 use std::hash::Hash;
 
-pub type HashFnameMap<T> = HashMap<T, Vec<String>>;
-pub type HashFn<T> = dyn Fn(&[u8]) -> T + Send + Sync + 'static;
+// pub type HashFnameMap<T> = HashMap<T, Vec<String>>;
+// pub type HashFn<T> = dyn Fn(&[u8]) -> T + Send + Sync + 'static;
 
-#[async_recursion]
-pub async fn md5_file_map<'a, T>(
-    map: &'a mut HashFnameMap<T>,
-    path: &Path,
-    hash_fn: &HashFn<T>,
-) -> Result<&'a mut HashFnameMap<T>, Box<dyn Error>>
+#[async_recursion(?Send)]
+pub async fn hash_files<T, F>(
+    path: impl AsRef<Path> + 'async_recursion,
+    hash_fn: &'async_recursion dyn Fn(DirEntry) -> F,
+) -> Result<(), Box<dyn Error + Send>>
 where
-    T: Hash + Eq + Send + Sync,
+    T: Hash + Eq + Send + Sync + Debug,
+    F: Future<Output = T>,
 {
-    println!("{:?}", path.file_name());
-    let mut entries = fs::read_dir(path).await?;
+    let mut entries = fs::read_dir(path).await.unwrap();
     while let Some(entry) = entries.next().await {
-        let next_path = entry?.path();
+        let next_path = entry.as_ref().unwrap().path();
         if next_path.is_dir().await {
-            md5_file_map(map, next_path.as_path(), hash_fn).await?;
+            hash_files(next_path.as_path(), &hash_fn).await.unwrap();
         } else {
-            let fcontents = fs::read(&next_path).await?;
-            let fdigest = hash_fn(&fcontents[..]);
-            let entry = map.entry(fdigest).or_insert(Vec::new());
-            entry.push(format!("{}", next_path.as_path().to_str().unwrap()));
+            let fname = format!("{}", next_path.as_path().to_str().unwrap());
+            let fdigest = hash_fn(entry.unwrap()).await;
+            println!("{}: {:#?}", fname, fdigest);
         }
     }
-    Ok(map)
+    Ok(())
 }
 
 mod test {
@@ -42,9 +42,11 @@ mod test {
 
     #[test]
     fn test_md5_file_map() {
-        let mut fhm: HashFnameMap<md5::Digest> = HashMap::new();
         let path = Path::new("./target");
-        task::block_on(md5_file_map(&mut fhm, &path, &|e: &[u8]| md5::compute(e))).unwrap();
-        println!("{:#?}", fhm);
+        task::block_on(hash_files(&path, &|e: DirEntry| async move {
+            let contents = fs::read(e.path()).await.unwrap();
+            md5::compute(contents)
+        }))
+        .unwrap();
     }
 }
