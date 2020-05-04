8858db2b2dbc85cb6f9547742128f5b51e5a27a2
Robin Kautz <mail@bob3000.net>
2020-05-04 18:54:36 +0200

    added channels

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,14 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Debug;
 use std::hash::Hash;
+use std::sync::mpsc;
 
-// pub type HashFnameMap<T> = HashMap<T, Vec<String>>;
-// pub type HashFn<T> = dyn Fn(&[u8]) -> T + Send + Sync + 'static;
+pub type HashFnameMap<T> = HashMap<T, Vec<String>>;
+pub struct FileHash<T: Hash + Eq + Send + Sync>(Option<T>, Option<String>);
 
 #[async_recursion(?Send)]
 pub async fn hash_files<T, F>(
+    sender: mpsc::Sender<FileHash<T>>,
     path: impl AsRef<Path> + 'async_recursion,
     hash_fn: &'async_recursion dyn Fn(DirEntry) -> F,
 ) -> Result<(), Box<dyn Error + Send>>
@@ -26,11 +28,13 @@ where
     while let Some(entry) = entries.next().await {
         let next_path = entry.as_ref().unwrap().path();
         if next_path.is_dir().await {
-            hash_files(next_path.as_path(), &hash_fn).await.unwrap();
+            hash_files(sender.clone(), next_path.as_path(), &hash_fn)
+                .await
+                .unwrap();
         } else {
             let fname = format!("{}", next_path.as_path().to_str().unwrap());
             let fdigest = hash_fn(entry.unwrap()).await;
-            println!("{}: {:#?}", fname, fdigest);
+            sender.send(FileHash(Some(fdigest), Some(fname))).unwrap();
         }
     }
     Ok(())
@@ -43,10 +47,20 @@ mod test {
     #[test]
     fn test_md5_file_map() {
         let path = Path::new("./target");
-        task::block_on(hash_files(&path, &|e: DirEntry| async move {
+        let (sender, receiver) = mpsc::channel();
+        task::block_on(hash_files(sender, &path, &|e: DirEntry| async move {
             let contents = fs::read(e.path()).await.unwrap();
             md5::compute(contents)
         }))
         .unwrap();
+
+        let mut hash_fname_map: HashFnameMap<md5::Digest> = HashMap::new();
+        while let Some(file_hash) = receiver.recv().iter_mut().next() {
+            let key = file_hash.0.take().unwrap();
+            let val = file_hash.1.take().unwrap();
+            let entry = hash_fname_map.entry(key).or_insert(Vec::new());
+            entry.push(val);
+        }
+        println!("{:#?}", hash_fname_map);
     }
 }
