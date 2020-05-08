7a592cab6f40223424445c0cb7152e9548d2aaf5
bob3000 <mail@bob3000.net>
2020-05-08 17:22:47 +0200

    added simple progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 mod lib;
-use async_std::path::Path;
 use async_std::task;
-use lib::{md5_file_map, HashFnameMap};
-use std::collections::HashMap;
+use structopt::StructOpt;
+use std::path::PathBuf;
+use indicatif::ProgressBar;
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+}
+
 
 fn main() {
-    let mut fhm: HashFnameMap<md5::Digest> = HashMap::new();
-    let path = Path::new("./target");
-    task::block_on(md5_file_map(&mut fhm, &path, &|e: &[u8]| md5::compute(e))).unwrap();
-    println!("{:#?}", fhm);
+    let args = Cli::from_args();
+    println!("{:?}", args);
+
+    let num_files = task::block_on(lib::count_files(&args.path)).unwrap();
+
+    let progress = ProgressBar::new(num_files);
+    task::block_on(
+        lib::md5_hash_files(&args.path, |hf| {
+            let results: Vec<Vec<String>> = hf.unwrap().into_iter().filter(|(_, v)| {
+                v.len() > 1
+            })
+            .map(|(_, v)| v)
+            .collect();
+
+            for res in results.iter() {
+                for duplicate in res.iter() {
+                    println!("{}", duplicate);
+                }
+                println!();
+            }
+        }, move || {progress.inc(1)})
+    );
 }
